538fc6b955e10db7ce1ee653e11017db223a3b3d
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-04-16 10:10:19 -0300

    Finalização do projeto - withdraw

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function operation() {
       } else if (action === 'Consultar saldo') {
         getAccountBalance();
       } else if (action === 'Sacar') {
+        withdraw();
       } else if (action === 'Sair') {
         console.log(
           chalk.bgCyan.black(
@@ -193,4 +194,65 @@ function getAccountBalance() {
       operation();
     })
     .catch((err) => console.log(err));
+}
+
+// withdraw
+
+function withdraw() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Qual o nome da sua conta?',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer['accountName'];
+
+      if (!checkAccount(accountName)) {
+        return withdraw();
+      }
+
+      inquirer
+        .prompt([
+          {
+            name: 'amount',
+            message: 'Quanto você deseja sacar?',
+          },
+        ])
+        .then((answer) => {
+          const amount = answer['amount'];
+          removeAmount(accountName, amount);
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+}
+
+function removeAmount(accountName, amount) {
+  const accountData = getAccount(accountName);
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'),
+    );
+    return withdraw();
+  }
+
+  if (accountData.balance < amount) {
+    console.log(chalk.bgRed.black('Valor Indisponivel'));
+    return withdraw();
+  }
+
+  accountData.balance = parseFloat(accountData.balance) - parseFloat(amount);
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    function (err) {
+      console.log(err);
+    },
+  );
+  console.log(
+    chalk.green(`Foi realizado um saque de R$ ${amount} na sua conta`),
+  );
+  operation();
 }
